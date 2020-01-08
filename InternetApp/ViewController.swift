99ad7d072e015d5fb006f169c6a5c3410ff6794d
Ben Faerber <benfaerber@protonmail.com>
2020-01-08 11:35:06 -0700

    Initial commit

---
+++ InternetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InternetApp
+//
+//  Created by Faerber, William on 1/8/20.
+//  Copyright © 2020 Faerber, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
